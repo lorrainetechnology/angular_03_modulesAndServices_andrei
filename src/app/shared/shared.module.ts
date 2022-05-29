0965292e845fdfbc99c43fc15f2d3b7afd360188
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-29 17:10:53 +0530

    modules and services

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalService } from './../services/modal.service';
+import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
+  ],
+  providers: [
+    ModalService
+  ]
+})
+export class SharedModule { }
